160ad7eba309cbcb524a08f3ed8c52a4bc7cc6e8
Saint-Gabriel26 <140563082+Saint-Gabriel26@users.noreply.github.com>
2023-08-01 19:30:00 -0400

    Update equites.h

--- equites.h
+++ equites.h
@@ -5,4 +5,7 @@ typedef struct t_equite equite;
 
 typedef struct t_liste_equite liste_equite;
 
+liste_equite* analyse_valeur_nette_totale(equite* actifs, int nbr_actifs, equite* passifs, int nbr_passifs);
+
+
 #endif //TP2_EQUITES_H
