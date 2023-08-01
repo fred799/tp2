d9ff6adc323a9568d5d52ec251b05e5d88c5feb6
Saint-Gabriel26 <140563082+Saint-Gabriel26@users.noreply.github.com>
2023-08-01 19:17:18 -0400

    Update actifs (1).h

--- actifs (1).h
+++ actifs (1).h
@@ -6,6 +6,7 @@ typedef struct t_actifs actifs;
 typedef struct t_liste_actifs liste_actifs;
 
 int nb_actif(Actif *liste_actifs[], int taille);
+Actif* charger_actif(const char* nom_fichier);
 
 void tester_nb_actif();
 
