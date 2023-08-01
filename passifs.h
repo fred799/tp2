0e9b7356a20b8aefd309dce7902591c8a395da07
Saint-Gabriel26 <140563082+Saint-Gabriel26@users.noreply.github.com>
2023-08-01 19:28:13 -0400

    Update passifs.h

--- passifs.h
+++ passifs.h
@@ -1,12 +1,13 @@
 #ifndef TP2_PASSIFS_H
 #define TP2_PASSIFS_H
 
-typedef struct passif t_passif;
-typedef struct liste_passif t_liste_passif;
+typedef struct t_passif passifs;
+typedef struct t_liste_passif liste_passifs;
 
 int nb_passif(liste_passif* liste_passifs);
 liste_passif* charger_passif(const char* nom_fichier);
 
 void tester_nb_passif();
 void tester_charger_passif();
+
 #endif //TP2_PASSIFS_H
