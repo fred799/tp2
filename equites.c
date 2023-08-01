380e737acb0e34f6b4ab8f731e0f6eb82efe5dfc
Saint-Gabriel26 <140563082+Saint-Gabriel26@users.noreply.github.com>
2023-08-01 18:25:25 -0400

    Update equites.c

--- equites.c
+++ equites.c
@@ -1,1 +1,17 @@
 #include "equites.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+struct t_equite {
+    char cle_unique[6];
+    char description[50];
+    double val_net;
+};
+
+struct liste_equite {
+    equite* tableau;
+    int nbr_case;
+};
+
