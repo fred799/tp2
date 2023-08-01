115213142f7d37ffa00e7b7884de1f16b7867138
Saint-Gabriel26 <140563082+Saint-Gabriel26@users.noreply.github.com>
2023-08-01 19:20:13 -0400

    Update actifs (1).h

--- actifs (1).h
+++ actifs (1).h
@@ -8,6 +8,7 @@ typedef struct t_liste_actifs liste_actifs;
 int nb_actif(Actif *liste_actifs[], int taille);
 Actif* charger_actif(const char* nom_fichier);
 char** obtenir_passifs_liés(Actif* liste_actifs, int indice_actif, int* nb_passifs_lies);
+float valeur_passifs_lier_actif(Actif *actifs, int indice_actif);
 
 void tester_nb_actif();
 void tester_charger_actif();
