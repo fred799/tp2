e918bc5d31f47846916bac74559e055e169743de
Saint-Gabriel26 <140563082+Saint-Gabriel26@users.noreply.github.com>
2023-08-01 20:42:57 -0400

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,17 @@
 #include "actifs.h"
 #include "equites.h"
 
+/***************************************
+    *** Déclarations des fonctions ***
+***************************************/
+// Déclaration de la fonction
+void enregistrer_analyser_valeur_nette(struct t_liste_equite* liste, const char* nom_fichier);
+
+// Procédure de test pour la fonction enregistrer_analyser_valeur_nette
+void test_enregistrer_analyser_valeur_nette();
+
+
+
 int main(void) 
 {
     
