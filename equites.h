369eac7d27ae0b12100bf58adff9d5ff12be3d71
Saint-Gabriel26 <140563082+Saint-Gabriel26@users.noreply.github.com>
2023-08-01 20:41:11 -0400

    Update equites.h

--- equites.h
+++ equites.h
@@ -7,5 +7,10 @@ typedef struct t_liste_equite liste_equite;
 
 liste_equite* analyse_valeur_nette_totale(equite* actifs, int nbr_actifs, equite* passifs, int nbr_passifs);
 
+// Déclaration de la fonction
+void enregistrer_analyser_valeur_nette(struct t_liste_equite* liste, const char* nom_fichier);
+
+// Procédure de test pour la fonction enregistrer_analyser_valeur_nette
+void test_enregistrer_analyser_valeur_nette();
 
 #endif //TP2_EQUITES_H
